75f5de9e2f752df99b95e27a57251b1f1ab5f298
nickyzj <reg.zj@outlook.com>
2016-03-29 16:45:24 +0800

    Changes to be committed: 	new file:   app.py 	modified:   flask-first-notes.txt 	new file:   project/__init__.py 	new file:   project/home/__init__.py 	new file:   project/home/views.py 	new file:   requirements.txt
    Create the blueprint for project/home

---
+++ project/home/views.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+
+home_blueprint = Blueprint(
+    'home', __name__,
+    template_folder='templates'
+)
+
+@home_blueprint.route('/blueprint')
+def home():
+	return "Hello Blueprint!"
